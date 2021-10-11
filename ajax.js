7e2898c8bf55d690574ea2dab6c15f2b973c414a
Tamara Cacheri <pedidos.littlesweet@gmail.com>
2021-10-11 18:08:45 -0300

    tercer commit

--- ajax.js
+++ ajax.js
@@ -7,6 +7,9 @@ $.getJSON(PROJSON, function (respuesta, estado) {
         $("#productosContent").append(`<div class="tarjetaProducto col">
                                 <img src="${dato.imagen}"></img>
                                 <h4>${dato.nombre}</h4>
+                                <ul>
+                                  <li>${dato.desc}</li>
+                                </ul>
                                 <h5>Precio de lista: $ ${dato.precio}</h5>
                                 <button type="button" class="btn btn-primary" id="btnAdidasSuperstar">Agregar al carrito</button>
                             </div>`)
